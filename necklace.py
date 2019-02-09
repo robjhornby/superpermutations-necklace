315ec506ee7375469db0bb21b425cbf8608b0395
Rob Hornby <robjhornby@gmail.com>
2019-02-09 22:11:07 +0000

    Showing that pruning will work with this iterative algorithm

--- necklace.py
+++ necklace.py
@@ -1,9 +1,7 @@
-##Generates all necklaces in two beads
-##using the algorithm which doesn't involve prenecklaces
-##and checks them for truth
-
+## Generates all necklaces iteratively
 
 import math
+import random
 
 class necklace:
     def __init__(self,k,n):
@@ -18,41 +16,38 @@ def divisors(n):
     div.add(n)
     return div
 
-def nextCyclicOld(touch, calltypes):
-    divs = divisors(len(touch))
-    t = list(touch)
-    while t != len(t)*[0]:
-        for pos, call in enumerate(t): # for each position in the string
-            if call > 0: # If this isn't the k'th character
-                t[pos] = call - 1 # set t to the next character
-                for i in range(0, pos): # for every position from pos to the end
-                    t[pos-i-1] = t[len(touch)-i-1] # copy characters from earlier to pos
-                    #Check graph at every step here
-                    
-                # condition for this to be a necklace
-                if len(t)-pos in divs:
-                    yield list(t)
-                
-                break
+def prune(t,pos):
+    if random.uniform(0,1)<0.1:
+        print('prune')
+        return True
+    return False
 
 def nextCyclic(size, n):
     divs = divisors(n)
     t = [0]*n
     while t != [size]*n:
-        for pos, el in reversed(tuple(enumerate(t))): # for each position in the string
-            if el < size: # If this isn't the k'th character
-                t[pos] = el + 1 # set t to the next character
+        for pos, el in reversed(tuple(enumerate(t))):
+            if el < size:
+                t[pos] = el + 1
+                show(t,pos)
+                if prune(t,pos):
+                    continue
                 
-                for i in range(n-pos-1): # for every position from the end to pos
-                    t[pos+i+1] = t[i] # copy characters from earlier to pos
-                    #Check graph at every step here
+                for i in range(n-pos-1): 
+                    t[pos+i+1] = t[i] # repeat the prenecklace to the end
+                    show(t,pos+i+1)
+                    if prune(t,pos+i+1):
+                        break
                     
                 # condition for this to be a necklace
                 if pos+1 in divs:
                     yield list(t)
                 
                 break
-
+def show(t,pos):
+    print(''.join([str(x) for x in t]))
+    print(' '*(pos)+'|')
+    input()
 
 def main():
     k = 1
@@ -61,13 +56,14 @@ def main():
     counter = 1
     ac = [(0,)*n]
     for calls in nextCyclic(k,n):
-        callsOld = next(nextCyclicOld(callsOld,k))
         ac.append(tuple(calls))
         counter = counter+1
         """print(calls)
         print(tuple(reversed(tuple(k-x for x in list(callsOld)))))
         print()"""
     print(counter)
+
+    
 if __name__ == '__main__':
     print('main')
     main()
